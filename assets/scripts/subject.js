dfa61059d7a99d9018e967e520993de013abb495
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2020-09-07 20:37:31 +0800

    Searchable Chips

--- assets/scripts/subject.js
+++ assets/scripts/subject.js
@@ -1,4 +1,5 @@
 const searchBox = document.querySelector("#searchBox");
+const searchChips = document.querySelector("#searchChips");
 const subject = new URLSearchParams(window.location.search).get("subject");
 const holder = document.querySelector(".wrapper");
 if (!subject) window.location.href = "/";
@@ -16,27 +17,60 @@ async function show(questionsAndAnswers) {
     }
 }
 
-async function main() {
+function search(keyword) {
+    if (keyword) {
+        let copy = [...allQuestionsAndAnswers].filter((qA) =>
+            JSON.stringify(qA).includes(keyword)
+        );
+        show(copy);
+    } else {
+        show(allQuestionsAndAnswers);
+    }
+}
+
+async function initContent() {
     var response = await fetch(`/assets/source/${subject}.json`);
     var json = await response.json();
     allQuestionsAndAnswers = json;
     show(allQuestionsAndAnswers);
+    console.log("Fetched and Loaded");
+}
+
+async function initSearch() {
+    function initSearchSuggestions() {
+        let allTags = [];
+        allQuestionsAndAnswers.forEach((qA) => {
+            const { tags } = qA;
+            if (tags) allTags = [...allTags, ...tags];
+        });
+        allTags = [...new Set(allTags)];
+
+        allTags.forEach((tag) => {
+            const base = document.createElement("div");
+            base.classList.add("search-chip");
+
+            const content = document.createElement("span");
+            content.textContent = tag;
+            base.append(content);
+
+            base.addEventListener("click", (e) => {
+                searchBox.value = tag;
+                search(searchBox.value);
+            });
+            searchChips.append(base);
+        });
+    }
+
+    searchBox.addEventListener("input", async (e) => {
+        search(searchBox.value);
+    });
+    initSearchSuggestions();
+    console.log("Search ready");
 }
 
 async function init() {
-    await main();
-    console.log("Fetched");
-    searchBox.addEventListener("input", async (e) => {
-        if (searchBox.value) {
-            let copy = [...allQuestionsAndAnswers].filter((qA) =>
-                JSON.stringify(qA).includes(searchBox.value)
-            );
-            show(copy);
-        } else {
-            show(allQuestionsAndAnswers);
-        }
-    });
-    console.log("Search ready");
+    await initContent();
+    await initSearch();
 }
 
 init();
