2c087ce3c5950cb391bcf855efe27d652c363b50
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-02-09 13:08:52 +0800

    Translated card

--- components/Explanation/ResultCard.tsx
+++ components/Explanation/ResultCard.tsx
@@ -1,4 +1,6 @@
+import { translate } from "@/locales/index";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import YouTube from "react-youtube";
 
@@ -6,7 +8,9 @@ interface Props {
     data: { [key: string]: any };
 }
 
-const ExplanationResultCard = ({ data }) => {
+const ExplanationResultCard = ({ data }: Props) => {
+    const router = useRouter();
+    const { locale } = router;
     const { subject, exam, year, paper, question, videos } = data;
     return (
         <Link
@@ -15,7 +19,16 @@ const ExplanationResultCard = ({ data }) => {
             )}`}
         >
             <a className="shadow-lg rounded-lg px-8 py-4 ring-2 ring-gray-300 flex-grow hover:shadow-xl active:bg-gray-200 hover:z-10 transition">
-                <div className="text-xl font-bold">{`${exam} ${subject} ${year} ${paper} ${question}`}</div>
+                <div className="text-xl font-bold">{`${translate(
+                    locale,
+                    `${exam}`
+                )} ${translate(locale, `${subject}`)} ${translate(
+                    locale,
+                    `${year}`
+                )} ${translate(locale, `${paper}`)} ${translate(
+                    locale,
+                    `${question}`
+                )}`}</div>
                 <div>{videos?.length} video(s)</div>
             </a>
         </Link>
